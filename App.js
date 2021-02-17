3da621ba4f94e89432e6c1143807e3667bd55a17
Manav Nair <55587634+manavnair2000@users.noreply.github.com>
2021-02-17 14:37:36 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import React,{ useState }  from 'react';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 export default function App() {
+  const [person, setPerson] =useState({name: 'Somu',age :'N/A'});
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Enter Name:</Text>
+      <TextInput multiline placeholder="e.g. Somu"  
+      style={styles.inputField} 
+      onChangeText={(val)=>{ setPerson({name:val, age:person.age})}}/>
+      <Text>Enter Age:</Text>
+      <TextInput multiline placeholder="e.g. 69" 
+        style={styles.inputField} keyboardType='numeric' onChangeText={(val) => { setPerson({ name: person.name, age: val }) }}/>
+      <Text>Hello, World! I am {person.name} and I am of {person.age} years old</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +26,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  inputField:{
+    borderWidth :1,
+    borderColor: 'red',
+    padding: 8,
+    width : 180
+  }
 });
